d96bf3932dd704f9093222eb56e2bf1e0727d3f8
Naya252 <52450582+Naya252@users.noreply.github.com>
2019-12-21 00:34:26 +0300

    reg

--- js/reg.js
+++ js/reg.js
@@ -1,12 +1,20 @@
 $(document).ready(function () {
 
     // <!-- Табы на JS -->
-    // $(".dws-form").on("click", ".tab", function () {
+    $(".dws-form").on("click", ".tab", function () {
     //    Удаляем классы active
-    //     $(".dws-form").find(".active").removeClass("active");
+        $(".dws-form").find(".active").removeClass("active");
 
     //    Добавляем класс active
-    //     $(this).addClass("active");
-    //     $(".tab-form").eq($(this).index()).addClass("active");
-    // });
+        $(this).addClass("active");
+        $(".tab-form").eq($(this).index()).addClass("active");
+    });
+    $(".dws-form2").on("click", ".tab2", function () {
+        //    Удаляем классы active
+            $(".dws-form2").find(".active").removeClass("active");
+    
+        //    Добавляем класс active
+            $(this).addClass("active");
+            $(".tab-form2").eq($(this).index()).addClass("active");
+        });
 });
