68ac9844109b404ab691e6ec751a472f2cf2c77c
Naya252 <52450582+Naya252@users.noreply.github.com>
2019-12-22 17:13:52 +0300

    desc
    desc

---
+++ js/mobile.js
@@ -0,0 +1,18 @@
+//изменения главной страницы при разрешении 769 px
+let h1 = document.getElementById("h1");
+
+// media query event handler
+if (matchMedia) {
+    const mqR = window.matchMedia("(max-width: 400px)");
+    mqR.addListener(WidthChange);
+    WidthChange(mqR);
+}
+	
+// media query change
+function WidthChange(mqR) {
+	if (mqR.matches) {
+        h1.style.display = "none";
+	} else {
+		h1.style.display = null;
+	}	
+}
